318ba85f9c04c75595a7e9d7c85df52e7718ab47
Avtex19 <atukaberadze19@gmail.com>
2024-09-16 15:36:11 +0400

    completed Logout(blacklisted refresh token)

--- back-end/myproject/myapp/api/urls.py
+++ back-end/myproject/myapp/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('', views.getRoutes),
     path('register/', views.userRegisterView, name="register"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+
 ]
