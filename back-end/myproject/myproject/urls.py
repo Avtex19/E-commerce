5dc145709cc230f31884e82707c8b8b35c1e5e39
Avtex19 <atukaberadze19@gmail.com>
2024-10-12 17:03:50 +0400

    feat:Add Swagger documentation

--- back-end/myproject/myproject/urls.py
+++ back-end/myproject/myproject/urls.py
@@ -17,6 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+       openapi.Info(
+          title="E-Commerce API",
+          default_version='v1',
+          description="API documentation",
+       ),
+       public=True,
+       permission_classes=(permissions.AllowAny,),
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +38,9 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+
