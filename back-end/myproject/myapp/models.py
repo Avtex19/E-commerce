d8ab8aaf3a9647c1ad04bf490da84721751f9a95
Avtex19 <atukaberadze19@gmail.com>
2024-10-12 18:17:07 +0400

    fix: create models: Category, Subcategory and Product. register them in admin.py

--- back-end/myproject/myapp/models.py
+++ back-end/myproject/myapp/models.py
@@ -9,3 +9,41 @@
 # def create_auth_token(sender, instance=None, created=False, **kwargs):
 #     if created:
 #         Token.objects.create(user=instance)
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Subcategories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    subcategory = models.ForeignKey(SubCategory, related_name='products', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Products'
