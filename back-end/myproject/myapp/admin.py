32bfa91a9d52f97173ee7e3b48cdf703e2c83ca4
Avtex19 <atukaberadze19@gmail.com>
2024-10-12 18:39:14 +0400

    Fix ProductAdmin to display category name with custom method
    - Add 'get_category_name' method in ProductAdmin to display the category name in the admin list.
    - Rename method to avoid conflict with the 'category' field.
    - Set custom label 'Category' for the method in the admin list.
    - Register SubCategoryAdmin for better display of SubCategory objects in the admin panel.

--- back-end/myproject/myapp/admin.py
+++ back-end/myproject/myapp/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Product,Category,SubCategory
+from .models import Product, Category, SubCategory
+
 
 # Register your models here.
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'subcategory', 'get_category_name')
+
+    def get_category_name(self, obj):
+        return obj.subcategory.category.name
+    get_category_name.short_description = 'category'
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
-admin.site.register(SubCategory)
+admin.site.register(SubCategory, SubCategoryAdmin)
