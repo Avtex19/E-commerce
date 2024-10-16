70820209665763e5b4c0cabb4fa9ea3373bab037
Avtex19 <atukaberadze19@gmail.com>
2024-10-16 22:21:11 +0400

    Feat: Implement products path

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import { Container } from '@mui/material';
 import LoginPage from './pages/LoginPage';
-import Header from './components/Header';
 import RegisterPage from "./pages/RegisterPage";
+import ProductPage from "./pages/ProductsPage";
 
 const App: React.FC = () => {
-    const location = useLocation();
 
     return (
         <>
-            {location.pathname !== '/login' && location.pathname !== '/register' && <Header />}
-
-
             <Container>
                 <Routes>
+                    <Route path="/" element={<ProductPage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                 </Routes>
