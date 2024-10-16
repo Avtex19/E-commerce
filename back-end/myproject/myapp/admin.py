186b9fbd296a1a6bb94ef1a95b9cc9fb886cb714
Avtex19 <atukaberadze19@gmail.com>
2024-10-16 15:47:23 +0400

    Fix: reformat file

--- back-end/myproject/myapp/admin.py
+++ back-end/myproject/myapp/admin.py
@@ -6,11 +6,13 @@ from .models import Product, Category, SubCategory
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'category')
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'subcategory', 'get_category_name')
 
     def get_category_name(self, obj):
         return obj.subcategory.category.name
+
     get_category_name.short_description = 'category'
 
 
