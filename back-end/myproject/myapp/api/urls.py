4667aa452f7e52a3078db1921068033aec8a3173
Avtex19 <atukaberadze19@gmail.com>
2024-10-28 00:31:04 +0400

    fix: instead of category/, write categories/

--- back-end/myproject/myapp/api/urls.py
+++ back-end/myproject/myapp/api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
-    path('category/',CategoriesView.as_view(), name="category"),
+    path('categories/',CategoriesView.as_view(), name="category"),
     # path('products/', ProductWithoutCategoryView.as_view(), name="productWithoutCategory"),
     path('account/update', UpdateAccountView.as_view()),
     path('user/info/', UserViewSet.as_view({'get': 'retrieve'}), name='user_info'),
