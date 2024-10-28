5df2d9dc096309112b38ad006e745a73d7db833f
Avtex19 <atukaberadze19@gmail.com>
2024-10-29 01:05:09 +0400

    feat: implement new route for products detail page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from '@mui/material';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from "./pages/RegisterPage";
 import ProductPage from "./pages/ProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage.tsx";
 
 const App: React.FC = () => {
 
@@ -13,7 +14,10 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<ProductPage />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/register" element={<RegisterPage/>}/>
+                    <Route path="/products/:id" element={<ProductDetailsPage />} />
+
+
                 </Routes>
             </Container>
         </>
