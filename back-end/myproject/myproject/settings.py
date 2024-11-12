144312f5b83fa32b0ddbbd9d7de225aad3c4865e
Avtex19 <atukaberadze19@gmail.com>
2024-11-12 13:51:56 +0400

    fix: change access and refresh token lifetimes

--- back-end/myproject/myproject/settings.py
+++ back-end/myproject/myproject/settings.py
@@ -101,8 +101,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=120),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
